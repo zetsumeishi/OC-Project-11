50d87db4423768e461ea02a4aa7cc034d92439ed
zetsumeishi <olivier.loustaunau@gmail.com>
2020-03-19 04:36:36 -0700

    Added apps to settings

--- pur_beurre/pur_beurre/settings.py
+++ pur_beurre/pur_beurre/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "www.apps.WwwConfig",
+    "product.apps.ProductConfig",
+    "account.apps.AccountConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
