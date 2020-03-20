cb215f8876d2af282a98da818e8730597dea6758
zetsumeishi <olivier.loustaunau@gmail.com>
2020-03-20 03:55:56 -0700

    Added basic Product model to test commands and CI/CD

--- pur_beurre/product/models.py
+++ pur_beurre/product/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=255)
+    url = models.URLField(max_length=255)
+    image_url = models.URLField(max_length=255)
+    nova_group = models.CharField(max_length=1)
+    nutriscore_grade = models.CharField(max_length=1)
+    saturated_fat_100g = models.DecimalField(
+        max_digits=6, decimal_places=3, null=True
+    )
+    fat_100g = models.DecimalField(max_digits=6, decimal_places=3, null=True)
+    salt_100g = models.DecimalField(max_digits=6, decimal_places=3, null=True)
+    sugars_100g = models.DecimalField(
+        max_digits=6, decimal_places=3, null=True
+    )
+
+    def __str__(self):
+        return self.product_name
