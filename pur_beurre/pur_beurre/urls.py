df440a0999f91147d3eafc8135c4f5dc490adcaf
zetsumeishi <olivier.loustaunau@gmail.com>
2020-03-31 10:03:25 -0700

    Changed url for admin panel

--- pur_beurre/pur_beurre/urls.py
+++ pur_beurre/pur_beurre/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", include("www.urls")),
     path("account/", include("account.urls")),
-    path("admin/", admin.site.urls),
+    path("becomeachef/", admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
