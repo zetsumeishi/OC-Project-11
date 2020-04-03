a646bb4cbb304fdeb23db48e12d32ff14e4c7093
zetsumeishi <olivier.loustaunau@gmail.com>
2020-04-03 06:34:36 -0700

    Added translation settings for Django strings

--- pur_beurre/pur_beurre/settings.py
+++ pur_beurre/pur_beurre/settings.py
@@ -107,7 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "fr-fr"
+
+LOCALE = "fr_FR"
 
 TIME_ZONE = "UTC"
 
