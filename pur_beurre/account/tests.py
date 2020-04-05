a60508b1fbf11a13946724bece151fb2edbc387b
zetsumeishi <olivier.loustaunau@gmail.com>
2020-04-05 06:02:28 -0700

    Fixed tests

--- pur_beurre/account/tests.py
+++ pur_beurre/account/tests.py
@@ -7,19 +7,24 @@ class AccountManagersTests(TestCase):
         self.User = get_user_model()
         self.email = "olivier@gmail.com"
         self.password = "p455w0rd"
+        self.first_name = "Olivier"
 
     def test_create_user(self):
         user = self.User.objects.create_user(
-            email=self.email, password=self.password
+            self.email, self.first_name, password=self.password,
         )
         self.assertEqual(user.email, self.email)
+        self.assertEqual(user.first_name, self.first_name)
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_admin)
         self.assertFalse(user.is_staff)
 
     def test_create_superuser(self):
-        user = self.User.objects.create_superuser(self.email, self.password)
+        user = self.User.objects.create_superuser(
+            self.email, self.first_name, password=self.password
+        )
         self.assertEqual(user.email, self.email)
+        self.assertEqual(user.first_name, self.first_name)
         self.assertTrue(user.is_active)
         self.assertTrue(user.is_admin)
         self.assertTrue(user.is_staff)
