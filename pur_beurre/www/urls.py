aa21befdc0ccaa485a9019aaa36d89a3bf00178b
zetsumeishi <olivier.loustaunau@gmail.com>
2020-05-25 07:45:28 -0700

    Adding legal notice page

--- pur_beurre/www/urls.py
+++ pur_beurre/www/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("mentions-legales/", views.legal_notice, name="legal-notice"),
 ]
--- pur_beurre/www/views.py
+++ pur_beurre/www/views.py
@@ -7,3 +7,9 @@ def home(request):
     context = {}
     context["search_form"] = SearchForm()
     return render(request, "www/home.html", context=context)
+
+
+def legal_notice(request):
+    context = {}
+    context["search_form"] = SearchForm()
+    return render(request, "www/legal-notice.html", context=context)
