12f71ba4aa4cd793a43b26acb352767829afb939
zetsumeishi <olivier.loustaunau@gmail.com>
2020-06-09 06:54:25 -0700

    Removed dead code

--- pur_beurre/account/forms.py
+++ pur_beurre/account/forms.py
@@ -14,20 +14,6 @@ class AccountCreationForm(forms.ModelForm):
         model = Account
         fields = ("email", "first_name")
 
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
-
 
 class AccountChangeForm(forms.ModelForm):
     password = ReadOnlyPasswordHashField()
@@ -35,9 +21,6 @@ class AccountChangeForm(forms.ModelForm):
     class Meta:
         model = Account
         fields = ("email", "password", "first_name", "is_active", "is_admin")
-
-    def clean_password(self):
-        return self.initial["password"]
 
 
 class SigninForm(forms.Form):
