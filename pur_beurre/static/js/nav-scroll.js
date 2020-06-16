bbfbf7b0e961e48163d6f5bea8dcbb4040537b80
zetsumeishi <olivier.loustaunau@gmail.com>
2020-06-16 13:08:15 +0200

    Added animations to navbar when scrolling

--- pur_beurre/static/js/nav-scroll.js
+++ pur_beurre/static/js/nav-scroll.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(window).scroll(function() {
-    if ($(document).scrollTop() > 80) {
+    if ($(document).scrollTop() > 50) {
       $('.navbar').addClass('affix');
     } else {
       $('.navbar').removeClass('affix');
