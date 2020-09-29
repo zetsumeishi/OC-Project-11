975f3a3017de4867a59da801c69c04cafad218a8
zetsumeishi <olivier.loustaunau@gmail.com>
2020-09-29 07:41:10 -0700

    Added tests for emails

--- www/tests.py
+++ www/tests.py
@@ -1,6 +1,13 @@
+import os
+
 from django.test import TestCase
 from django.test.client import Client
 from django.urls import reverse
+
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+from www.forms import ContactForm
 
 
 class WwwViewsTests(TestCase):
@@ -17,3 +24,31 @@ class WwwViewsTests(TestCase):
 
     def tearDown(self):
         pass
+
+
+class ContactFormTests(TestCase):
+    def test_contact_form(self):
+        form_data = {
+            "from_email": "olivier.loustaunau@gmail.com",
+            "subject": "Test subject",
+            "content": "Content of the email."
+        }
+        form = ContactForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        form_data = {"from_email": "not_an_email"}
+        form = ContactForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+
+class SendGridTests(TestCase):
+    def test_sendgrid(self):
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        message = Mail(
+            from_email='olivier.loustaunau@gmail.com',
+            to_emails='olivier.loustaunau@gmail.com',
+            subject='Sending with Twilio SendGrid is Fun',
+            html_content='Test content'
+        )
+        import ipdb; ipdb.set_trace()
+        response = sg.send(message)
+        self.assertEqual(response.status_code, 202)
