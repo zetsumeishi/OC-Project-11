43bdd99fdb66e5fc8005f21f0836756bacded500
zetsumeishi <olivier.loustaunau@gmail.com>
2020-11-11 10:58:25 -0800

    nicer form for emails

--- www/forms.py
+++ www/forms.py
@@ -2,6 +2,15 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    from_email = forms.EmailField(required=True)
-    subject = forms.CharField(required=True)
-    content = forms.CharField(widget=forms.Textarea, required=True)
+    from_email = forms.EmailField(
+        widget=forms.EmailInput(attrs={"placeholder": "email..."}),
+        required=True,
+    )
+    subject = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "sujet..."}),
+        required=True,
+    )
+    content = forms.CharField(
+        widget=forms.Textarea(attrs={"placeholder": "message..."}),
+        required=True,
+    )
